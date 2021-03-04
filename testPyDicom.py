09d23c61ed2da01fa38a73e773e1cef5a6d1ef5d
HP-Z4-G4 <xuchu_liu@rush.edu>
2021-03-04 14:58:13 -0600

    update .gitignore

--- testPyDicom.py
+++
@@ -1,8 +0,0 @@
-import pydicom
-import matplotlib.pyplot as plt
-
-filepath = './Patient_7626622/_jdeng__0215031641'
-ds = pydicom.dcmread(filepath + '/ser012img00001.dcm')
-plt.figure(figsize=(10,10))
-plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
-plt.show()
